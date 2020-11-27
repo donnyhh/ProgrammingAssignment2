efce68e5f19bc57cdac74cb2788fb8e5f31f7c74
donnyhh <74312128+donnyhh@users.noreply.github.com>
2020-11-27 11:54:09 +0800

    Update cachematrix.R
    input my script with comments for assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## a<-makeCacheMatrix()         to log a into the functions in makeCacheMatrix
+## a$set(matrix)                to set matrix in working environment
+## cacheSolve(a)                to return inverse matrix
 
-## Write a short comment describing this function
-
+## makeCacheMatrix sets certain functions to be used by cacheSolve
 makeCacheMatrix <- function(x = matrix()) {
-
+## creates i and set it as NULL
+        i <- NULL
+## creates set as a function to store value: y in cache: x
+## resets i to NULL in case other functions stored a value in i
+        set <- function(y) {
+            x<<-y
+            i<<-NULL
+        }
+## creates get as a function to return the matrix x
+  get<-function()x
+## creates setinv as a function to store inverse of i as inv
+  setinv<-function(inv)i<<-inv
+## creates getinv as a function to return the stored inverse value
+  getinv<-function()i
+## set the functions to list again so that the $set $get $setinv and $getinv can work for new values
+  list(set = set, get = get, setinv = setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve returns inverse of matrix x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## get inverse of matrix
+        i<-x$getinv()
+## return inverse of matrix, and show message if nothing is stored
+        if(!is.null(i)){
+            message("getting cached data")
+            return(i)
+         }
+## creates data to store the value returned by x$get()
+        data <- x$get()
+## perform the inverse function solve for data and store as i
+        i <- solve(data)
+## store the inverted x as i in cache
+        x$setinv(i)
+## returns the inverted value
+        i
 }
